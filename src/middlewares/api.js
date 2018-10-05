5927adc9a341ed52ebbf51cbe039c3db5be9a4f3
Denis Isaev <denis@isaev.store>
2018-10-05 14:30:30 +0300

    add immutable

--- src/middlewares/api.js
+++ src/middlewares/api.js
@@ -2,7 +2,9 @@ export default store => next => action => {
   const {callAPI} = action
   if (!callAPI) return next(action)
 
-  fetch(callAPI)
-    .then(res => res.json())
-    .then(response => next({...action, response}))
+  setTimeout(() => {
+    fetch(callAPI)
+      .then(res => res.json())
+      .then(response => next({...action, response}))
+  }, 3000);
 }
--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -1,18 +1,33 @@
+import {Map, Record} from 'immutable'
 import {articles as defaultArticles} from '../mock'
 import {arrToMap} from '../helpers'
 import {DELETE_ARTICLE, ADD_COMMENT, LOAD_ARTICLES} from '../constans'
 
+const defaultState  = new Map({}) 
 
-export default (articleState = {}, action) => {
+const ArticleRecord = Record({
+  text: undefined,
+  title: '',
+  comments: []
+}) 
+
+export default (articleState = defaultState, action) => {
   const {type, payload, response, randomId} = action
 
   switch(type) {
     case DELETE_ARTICLE: 
+      /* 
       const tmpState = {...articleState}
       delete tmpState[payload.id]
-      return tmpState
+      return tmpState 
+       ▼ Заменено с помощью immutable.js */
+      return articleState.delete(payload.id)
 
-    case ADD_COMMENT: 
+    case ADD_COMMENT:
+      /* Передаем по какому пути мы хотим поменять чтото ► [payload.articleId, 'comments'] ◄
+      как это что-то мы хотим поменять ► comments => comments.concat(randomId) ◄ */
+      return articleState.updateIn( [payload.articleId, 'comments'],  comments => comments.concat(randomId))
+      /* ▲ Заменено с помощью метода updateIn из пакета immutable.js 
       const article = articleState[payload.articleId]
       return {
         ...articleState,
@@ -20,10 +35,10 @@ export default (articleState = {}, action) => {
           ...article,
           comments: (article.comments || []).concat(randomId)
         }
-      }
+      } */
 
     case LOAD_ARTICLES: 
-      return arrToMap(response)
+      return arrToMap(response, ArticleRecord)
   }
   return articleState
 }
